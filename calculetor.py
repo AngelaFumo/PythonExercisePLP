dec88f2883562ec667fdcb5f8e16e38928da76f2
Angela Fumo <angelafumo75@gmail.com>
2025-08-02 06:16:16 +0200

    my first exercise od PLP in python

---
+++ calculetor.py
@@ -0,0 +1,8 @@
+n1= int(input("write first number"))
+n2= int(input("write second number"))
+addition= n1+n2
+subtraction= n1-n2
+multiplication= n1*n2
+division=n1/n2
+print("Addition of this number is: ",addition," Subtraction of this numbers is: ", subtraction,
+      "Multiplication of this numbers is: ", multiplication, " and division of this numbers is : ",division)
---
+++ index.py
@@ -0,0 +1,4 @@
+name= input("what is your name : ")
+cor = input("what is your favorite color : ")
+
+print("Mr./s: ", name, " your favorite color is : ",cor)
